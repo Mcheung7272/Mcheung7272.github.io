1afadb24147bc71a15a14a458d5331f67d27ecb0
Matthew C <mcheung3@albany.edu>
2022-04-17 12:07:58 -0400

    commit

---
+++ script.js
@@ -0,0 +1,37 @@
+window.onscroll = function (e) {
+    parallax();
+};
+
+function parallax() {
+    let scrolled = $(window).scrollTop();
+    let h1Opacity = 1 - (scrolled / 270);
+    let pOpacity = 1 - (scrolled / 390);
+    let bOpacity = 1 - (scrolled / 410);
+    let arrowOpacity = 1 - (scrolled / 450);
+    if (h1Opacity > 0) {
+        $('.hero-text h1').css('opacity', h1Opacity);
+    }
+
+    if (pOpacity > 0) {
+        $('.hero-text p').css('opacity', pOpacity);
+    }
+
+    if (bOpacity > 0) {
+        $('.hero-text button').css('opacity', bOpacity);
+    }
+
+    if (arrowOpacity > 0) {
+        $('.wrap .arrow').css('opacity', arrowOpacity);
+    }
+
+};
+
+function arrowOnClick() {
+    if (window.screen.availWidth > 800 && window.screen.availHeight > 1880) {
+        $("html, body").animate({ scrollTop: $(document).height() - $(window).height() }, 2000);
+    } else {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 2000);
+    }
+}
