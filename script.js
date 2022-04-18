1aa098fe18d9cbfe101e42e7314ffda7f5170be6
Matthew C <mcheung3@albany.edu>
2022-04-17 23:28:12 -0400

    added bootstrap grid

--- script.js
+++ script.js
@@ -19,11 +19,14 @@ $(window).scroll(function () {
 });
 
 function arrowOnClick() {
-    if (window.screen.availWidth > 800 && window.screen.availHeight > 1880) {
-        $("html, body").animate({ scrollTop: $(document).height() - $(window).height() }, 2000);
-    } else {
-        $('html, body').animate({
-            scrollTop: $("#about").offset().top
-        }, 2000);
-    }
+    // if (window.screen.availWidth > 800 && window.screen.availHeight > 1880) {
+    //     $("html, body").animate({ scrollTop: $(document).height() - $(window).height() }, 2000);
+    // } else {
+    //     $('html, body').animate({
+    //         scrollTop: $("#about").offset().top
+    //     }, 2000);
+    // }
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 300);
 }
