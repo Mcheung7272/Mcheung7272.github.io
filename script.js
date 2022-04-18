1cad63ac391ecf0353331c9e637184aa3bca3a90
Matthew C <mcheung3@albany.edu>
2022-04-17 21:28:39 -0400

    replaced scrolling opacity script

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
-window.onscroll = function (e) {
-    parallax();
-};
+// window.onscroll = function (e) {
+//     parallax();
+// };
 
-function parallax() {
-    let scrolled = $(window).scrollTop();
-    let h1Opacity = 1 - (scrolled / 270);
-    let pOpacity = 1 - (scrolled / 390);
-    let bOpacity = 1 - (scrolled / 410);
-    let arrowOpacity = 1 - (scrolled / 450);
-    if (h1Opacity > 0) {
-        $('.hero-text h1').css('opacity', h1Opacity);
-    }
+// function parallax() {
+//     let scrolled = $(window).scrollTop();
+//     let h1Opacity = 1 - (scrolled / 270);
+//     let pOpacity = 1 - (scrolled / 390);
+//     let bOpacity = 1 - (scrolled / 410);
+//     let arrowOpacity = 1 - (scrolled / 450);
+//     if (h1Opacity > 0) {
+//         $('.hero-text h1').css('opacity', h1Opacity);
+//     }
 
-    if (pOpacity > 0) {
-        $('.hero-text p').css('opacity', pOpacity);
-    }
+//     if (pOpacity > 0) {
+//         $('.hero-text p').css('opacity', pOpacity);
+//     }
 
-    if (bOpacity > 0) {
-        $('.hero-text button').css('opacity', bOpacity);
-    }
+//     if (bOpacity > 0) {
+//         $('.hero-text button').css('opacity', bOpacity);
+//     }
 
-    if (arrowOpacity > 0) {
-        $('.wrap .arrow').css('opacity', arrowOpacity);
-    }
+//     if (arrowOpacity > 0) {
+//         $('.wrap .arrow').css('opacity', arrowOpacity);
+//     }
 
-};
+// };
+
+$(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+
+    $('.hero-text').css({
+        opacity: function () {
+            var elementHeight = $(this).height(),
+                opacity = ((elementHeight - scrollTop) / elementHeight);
+            return opacity;
+        }
+    });
+
+    $('.wrap').css({
+        opacity: function () {
+            var elementHeight = $(this).height(),
+                opacity = ((elementHeight - scrollTop) / elementHeight);
+            return opacity;
+        }
+    });
+});
 
 function arrowOnClick() {
     if (window.screen.availWidth > 800 && window.screen.availHeight > 1880) {
